3307bc08c761cb3296eb59b751ab264982da0b5b
telexserve <108548542+telexserve@users.noreply.github.com>
2023-04-27 13:22:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ var fs = require('fs')
 fs.readFile('index.html','utf-8',function(err,data) {
     image({output:'tank.png',html:`'${data}'`})
 })
+
+
+const launchBrowser = async () => {
+    puppetBrowser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      timeout: 10000,
+    })
+  }
